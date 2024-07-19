3f398a9ffa513130dbfe49a0f5fa1e449af533df
Mostafa Elmansy <mostafaelmansy981125@gmail.com>
2024-07-19 19:47:16 +0300

    extend our parser again
    goal is to extend the parser to parse a JSON object with object and array values

--- Json_parser.py
+++ Json_parser.py
@@ -4,7 +4,7 @@ def lexer(input_string):
     tokens = []
     i = 0
     while i < len(input_string):
-        if input_string[i] == '{' or input_string[i] == '}' or input_string[i] == ':' or input_string[i] == ',':
+        if input_string[i] in ['{', '}', ':', ',', '[', ']']:
             tokens.append(input_string[i])
             i += 1
         elif input_string[i] == '"':
@@ -34,28 +34,68 @@ def lexer(input_string):
             raise ValueError(f"Unexpected character: {input_string[i]}")
     return tokens
 
+def parser_value(tokens, index):
+    if tokens[index] == '{':
+        return parser_object(tokens, index)
+    elif tokens[index] == '[':
+        return parser_array(tokens, index)
+    elif tokens[index].startswith('"') and tokens[index].endswith('"'):
+        return index + 1 # string value
+    elif tokens[index] in ['true', 'false', 'null']:
+        return index + 1 # boolean and null value
+    elif tokens[index].isdigit() or (tokens[index].startswith('-') and tokens[index][1:].isdigit()):
+        return index + 1 # numeric value
+    return -1
+
+
+def parser_object(tokens, index):
+    if tokens[index] != "{":
+        return -1
+    index += 1
+    expected_key = True
+    while index < len(tokens):
+        if tokens[index] == "}":
+            return index + 1 
+        if expected_key:
+            if not (tokens[index].startswith('"') and tokens[index].endswith('"')):
+                return -1
+            index += 1
+            if tokens[index] != ':':
+                return -1
+            index += 1
+            index = parser_value(tokens, index)
+            if index == -1:
+                return -1
+            expected_key = False
+        else:
+            if tokens[index] == ',':
+                index += 1
+                expected_key = True
+            elif tokens[index] == "}":
+                return index + 1 
+            else:
+                return -1
+    return -1 
+        
+
+def parser_array(tokens, index):
+    if tokens[index] != "[":
+        return -1
+    index += 1 
+    while index < len(tokens) and tokens[index] != ']':
+        index = parser_value(tokens, index)
+        if index == -1:
+            return -1
+        if tokens[index] == ',':
+            index += 1
+    if tokens[index] != ']':
+        return -1
+    return index + 1
+
 def parser(tokens):
     if tokens[0] != '{' or tokens[-1] != '}':
         return False
-
-    i = 1
-    expected_key = True
-    while i < len(tokens) - 1:
-        if expected_key:
-            if not (tokens[i].startswith('"') and tokens[i].endswith('"')):
-                return False
-            expected_key = False
-        else:
-            if tokens[i] != ':':
-                return False
-            if i + 1 >= len(tokens):
-                return False
-            if not (tokens[i + 1].startswith('"') and tokens[i+1].endswith('"')) and tokens[i + 1] not in ['true', 'false', 'null'] and not tokens[i + 1].isdigit() and not (tokens[i + 1].startswith('-') and tokens[i + 1][1:].isdigit()):
-                return False
-            expected_key = True
-            i += 2 # skip the value token
-        i += 1 # move to the next token
-    return True
+    return parser_object(tokens, 0) == len(tokens)
             
 
 def validate_json(input_string):
